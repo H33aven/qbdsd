b3028f0b89459b2635619b1d6c4aa4d2c1c2c2ee
Eto Dissapointed <Diavol0oRu@gmail.com>
2025-08-19 15:17:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,22 +6,21 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 
 handle_error() {
-    echo -e "${RED}Ошибка в строке $1: ${2}${NC}" >&2
+    echo -e "${RED}Error in line $1: ${2}${NC}" >&2
     exit 1
 }
 
 trap 'handle_error ${LINENO} "$BASH_COMMAND"' ERR
 
-echo -e "${YELLOW}Копирование конфигурационных файлов...${NC}"
+echo -e "${YELLOW}Loading...${NC}"
 
-cp -v files/waybar/styles/graphite.css ~/.local/share/waybar/styles/
-cp -v files/waybar/layouts/graphite.jsonc ~/.local/share/waybar/layouts/
+cp -v files/waybar/graphite.css ~/.local/share/waybar/styles/
+cp -v files/waybar/graphite.jsonc ~/.local/share/waybar/layouts/
 
 cp -v files/hypr/hyprlock.conf ~/.config/hypr/
 cp -v files/hypr/keyboard.conf ~/.config/hypr/
 cp -v files/hypr/hyprland.conf ~/.config/hypr/
 
-echo -e "${YELLOW}Распаковка темы Graphite_Eto...${NC}"
 unzip -o files/hyde/themes/Graphite_Eto.zip -d ~/.config/hyde/themes/
 
-echo -e "${GREEN}Установка завершена успешно!${NC}"
+echo -e "${GREEN}Succesfully!${NC}"

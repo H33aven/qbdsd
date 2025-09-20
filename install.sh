5f94d5568249a911e113de05226d5411a86071bf
Eto Dissapointed <Diavol0oRu@gmail.com>
2025-09-20 03:06:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo pacman -S unzip --noconfirm
+
 mkdir -p ~/.local/share/waybar/styles/
 mkdir -p ~/.local/share/waybar/layouts/
 mkdir -p ~/.config/hypr/
@@ -15,14 +17,8 @@ cp -v files/hypr/hyprland.conf ~/.config/hypr/
 cp -v files/hypr/keybindings.conf ~/.config/hypr/
 cp -v files/hypr/config.jsonc ~/.config/fastfetch
 
+unzip -o files/hyde/Nightfox-Dark-Carbonfox.zip -d ~/.themes/
 unzip -o files/hyde/Graphite_Eto.zip -d ~/.config/hyde/themes/
 cp -v files/hyde/hyde.conf ~/.local/share/hyde/
-
-sudo pacman -S discord steam --noconfirm
-yay -S spotify spicetify-cli
-
-sudo chmod a+wr /opt/spotify
-sudo chmod a+wr /opt/spotify/Apps -R
-curl -fsSL https://raw.githubusercontent.com/spicetify/marketplace/main/resources/install.sh | sh
 
 echo "Successfully!"
